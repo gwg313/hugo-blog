fe2ddd9e06c6ee961586987e95bab9a3f0b01b36
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-11-04 00:30:37 -0400

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Environment for my Hugo Blog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              hugo
+            ];
+          };
+        }
+      );
+}
