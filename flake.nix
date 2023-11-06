4a0b5d32709b26abdf07444dac2b85001da6ffc9
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-11-06 00:47:07 -0500

    updates

--- flake.nix
+++ flake.nix
@@ -1,22 +1,57 @@
 {
-  description = "Environment for my Hugo Blog";
-
+  description = "A flake for my hugo blog";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
+  outputs =
+    { self
+    , nixpkgs
+    , pre-commit-hooks
+    , ...
+    }:
+    let
+      withSystem = f:
+        nixpkgs.lib.fold nixpkgs.lib.recursiveUpdate { } (
+          map f [
+            "x86_64-linux"
+            "x86_64-darwin"
+            "aarch64-linux"
+            "aarch64-darwin"
+          ]
+        );
+    in
+    withSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        mdLint = pkgs.writeScriptBin "md-lint" ''
+          echo "Checking Markdown Formatting..."
+          mdl ./content/posts
+        '';
+      in
+      {
+        devShells.${system} = {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = [ mdLint ] ++ (with pkgs; [
               hugo
-            ];
+              mdl
+            ]);
           };
-        }
-      );
+        };
+        checks.${system} = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              markdownlint.enable = true;
+            };
+          };
+        };
+      }
+    );
 }
